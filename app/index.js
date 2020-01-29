f66e7cbb5784441417bb76b5729da9dcc06614e9
jkarelins <jkarelins@gmail.com>
2020-01-29 17:22:53 +0100

    JS added

--- app/index.js
+++ app/index.js
@@ -1,3 +1,22 @@
+const { artObjects } = data;
+const gallery = document.getElementById("gallery");
+
+for (artObject of artObjects) {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<a href="./pages/detail-page.html"><img
+      class="artObject"
+      src="${artObject.webImage.url}"
+      alt="${artObject.title}"
+  /></a>`
+  );
+}
+
+const art = document.getElementsByClassName("artObject");
+for (artObject of art) {
+  // showArt(artObject.id);
+}
+
 function newComment(e) {
   const nameInput = document.getElementById("nameInput");
   const commentText = document.getElementById("message");
