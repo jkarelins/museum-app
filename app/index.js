1caf45841909004e87d26bcec374117a4e072a00
jkarelins <jkarelins@gmail.com>
2020-01-30 14:08:29 +0100

    search by year and painter

--- app/index.js
+++ app/index.js
@@ -1,28 +1,52 @@
 const { artObjects } = data;
 const gallery = document.getElementById("gallery");
+let artAuthor = { name: "allArt" };
+let innerArtObj = [];
+let minYear = 0;
+let maxYear = 1500;
 
-// // Create index Gallery
-gallery.innerHTML += artObjects
-  .map(artObject => {
-    const { longTitle } = artObject;
-    const date = +longTitle.slice(-4);
-    if (
-      artObject.webImage.width > 1500 &&
-      artObject.principalOrFirstMaker !== "Gerard van Honthorst" &&
-      date < 1800
-    ) {
-      return `<a href="./pages/detail-page.html">
+// function to sort gallery
+function sortArtByAuth(author) {
+  if (artAuthor.name == "allArt") {
+    innerArtObj = artObjects;
+  } else {
+    innerArtObj = artObjects.filter(
+      artObj => author == artObj.principalOrFirstMaker
+    );
+  }
+}
+
+function sortByYear(minYear, maxYear) {
+  innerArtObj = innerArtObj.filter(
+    artObj =>
+      artObj.longTitle.slice(-4) < maxYear &&
+      artObj.longTitle.slice(-4) > minYear
+  );
+}
+
+// function to create gallery
+function createArtGallery() {
+  $(".artCover").remove();
+  gallery.innerHTML += innerArtObj
+    .map(artObject => {
+      // if (
+      // artObject.webImage.width > 1500 &&
+      // artObject.principalOrFirstMaker !== "Gerard van Honthorst" &&
+      // date < 1800
+      // author === "allArt"
+      // true
+      // ) {
+      return `<a href="./pages/detail-page.html" class='artCover'>
       <img
           class="artObject"
           data="${artObject.id}"
           src="${artObject.webImage.url}"
           alt="${artObject.title}"
 ></a>`;
-    }
-  })
-  .join("");
-
-// Join to remove extra commas from toString -> innerHTML uses
+      // }
+    })
+    .join("");
+}
 
 const artObjsArr = document.getElementsByClassName("artObject");
 
@@ -32,3 +56,35 @@ for (artObj of artObjsArr) {
     localStorage.setItem("artObjectToDetails", id);
   });
 }
+
+document.getElementById("painterSelector").innerHTML +=
+  '<option value="allArt">Show All</option>';
+
+document.getElementById("painterSelector").innerHTML += artObjects
+  .map(artObject => {
+    return `<option value="${artObject.principalOrFirstMaker}">${artObject.principalOrFirstMaker}</option>`;
+  })
+  .join("");
+
+function runApp() {
+  sortArtByAuth(artAuthor.name);
+  console.log(minYear, maxYear);
+  sortByYear(minYear, maxYear);
+  createArtGallery();
+  $("#painterSelector").change(e => {
+    artAuthor.name = e.target.value;
+    runApp();
+    $("#minYear").val(minYear);
+    $("#maxYear").val(maxYear);
+  });
+  $("#searchForYears").on("click", e => {
+    e.preventDefault();
+    minYear = $("#minYear").val();
+    maxYear = $("#maxYear").val();
+    runApp();
+    $("#minYear").val(minYear);
+    $("#maxYear").val(maxYear);
+  });
+}
+
+runApp();
